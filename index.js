d49fa6aa952030a678962d16164ceb56ad2dce21
Sosoかも <71214511+javascriptjp@users.noreply.github.com>
2022-09-06 16:28:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((request, response)=>{
     response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    response.end('<body><style>body{background-color:#141414;}</style>{ Hello World. }</body>');
+    response.end('<body><style>body{background-color:#141414;color:#ffffff;}</style>{ Hello World. }</body>');
 }).listen(3000);
